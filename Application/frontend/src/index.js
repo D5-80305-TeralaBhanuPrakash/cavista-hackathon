8083084d26d7a8f10d0bc98c80f7c0c2f7ff2b94
Terala_Bhanu_Prakash <teralabhanu2001@gmail.com>
2024-02-18 23:32:44 +0530

    application

--- Application/frontend/src/index.js
+++ Application/frontend/src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let persistor = persistStore(store);
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- Application/frontend/src/services/util.js
+++
@@ -1,9 +0,0 @@
-const SERVER_URL = 'http://localhost:8080'
-
-export function createError(error){
-    return {status:'error',error}
-}
-
-export function createUrl(path){
-    return `${SERVER_URL}/${path}`
-}
